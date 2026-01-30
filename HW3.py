402af499dd2c3ba8f1094f0a7bf938966c673d6d
Hally Levine <hallylevine@Hallys-MacBook-Air.local>
2026-01-30 12:05:34 -0500

    function 1

--- HW3.py
+++ HW3.py
@@ -1,6 +1,6 @@
-# Name:
-# Student ID:
-# Email:
+# Name: Hally Levine
+# Student ID: 76783278
+# Email: hally@umich.edu
 # Who or what you worked with on this homework (including generative AI like ChatGPT):
 # If you worked with generative AI also add a statement for how you used it.
 # e.g.:
@@ -33,7 +33,8 @@ class CouponDispenser:
             coupon_cards (list[str]): list of possible coupons users can receive.
         """
         # TODO: Implement per instructions
-        pass
+        
+        self.coupon_cards = coupon_cards
 
     def __str__(self):
         """
