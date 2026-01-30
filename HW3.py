e851372ceded5ecff565171b1c92390759b196a3
Hally Levine <hallylevine@Hallys-MacBook-Air.local>
2026-01-30 12:39:33 -0500

    function 3

--- HW3.py
+++ HW3.py
@@ -53,7 +53,22 @@ class CouponDispenser:
         pass
 
     def issue_coupon(self, name):
-        """
+
+        if len(self.coupon_cards) == 0: 
+            return "The box is empty"  
+        
+        if name in self.customer_roster: 
+            customer_index = self.customer_roster.index(name)
+            customer_coupon = self.coupon_cards(self.issued_indices(customer_index))
+            return f"That name already has a coupon: {customer_coupon}" 
+        
+        if name not in self.customer_roster:  #chatgpt helped me with part. I had no idea what I was doing wrong. 
+            rand_index = random.randrange(len(self.coupon_cards))
+            self.customer_roster.append(name)
+            self.issued_indices.append(rand_index)
+            return self.coupon_cards[rand_index]
+        """ 
+    
         Assign name with a random coupon. If name is already assigned a coupon, return it.
         If the list coupon_cards is empty, return:
         "The box is empty."
